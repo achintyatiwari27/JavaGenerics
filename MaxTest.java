e080ac3555478fcaa0ba2f7d5a38a99ba46c5938
Achintya <achintyatiwari27>
2021-09-16 17:45:47 +0530

    [Added]:Code to get max 3 float with test case

--- MaxTest.java
+++ MaxTest.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void intMax1() {
-        int actualValue = Maximum.getmaximum(1,5,10);
-        Assert.assertEquals(10,actualValue);
+        Float actualValue = Maximum.getmaximum(1.2f,5.7f,10.8f);
+        Assert.assertEquals(10.8,actualValue,0);
     }
 
     @Test
     public void intMax2() {
-        int actualValue = Maximum.getmaximum(77,55,60);
-        Assert.assertEquals(77,actualValue);
+        Float actualValue = Maximum.getmaximum(77f,55f,60f);
+        Assert.assertEquals(77,actualValue,0);
     }
     @Test
     public void intMax3() {
-        int actualValue = Maximum.getmaximum(4657,5405,1746);
-        Assert.assertEquals(5405,actualValue);
+        Float actualValue = Maximum.getmaximum(4657f,5405f,1746f);
+        Assert.assertEquals(5405,actualValue,0);
     }
 }
 
